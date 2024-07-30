5e3f0f39ff0b2363ec1f0d985838ce40ae70d736
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-07-30 01:33:07 -0500

    "Added two axios POST requests to localhost:4006 and localhost:4007 in /events endpoint"

--- events-bus/index.js
+++ events-bus/index.js
@@ -28,6 +28,14 @@ app.post('/events', (req, res) => {
         console.log(err.message)
     })
 
+    axios.post('http://localhost:4006/events', event).catch((err) => {
+        console.log(err.message)
+    })
+
+    axios.post('http://localhost:4007/events', event).catch((err) => {
+        console.log(err.message)
+    })
+
     res.send({status: 'OK'})
 })
 
